f84f4312198181bd2854ab98538915e813ba3e17
Geraud <geraud.pereira@hotmail.com>
2023-10-27 09:29:16 -0300

    Desafio 100 dias

---
+++ Dia_06.py
@@ -0,0 +1,12 @@
+print("SECURE LOGIN")
+print("------------")
+username = input("Username > ")
+password = input("Password > ")
+if username == "Geraud" and password == "1234":
+  print("Welcome Geraud!Have a great day!")
+else:
+  print("Wrong credentials")
+if username == "mark":
+  print("Welcome Mark!")
+elif username == "Geraud":
+  print("Welcome Geraud!")
