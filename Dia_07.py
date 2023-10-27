145dfc01eef8ba441d2f238919500219b893afb0
Geraud <geraud.pereira@hotmail.com>
2023-10-27 09:30:56 -0300

    Desafio 100 dias

---
+++ Dia_07.py
@@ -0,0 +1,18 @@
+print ("Vamos ver se você é realmente fã de glee")
+print ()
+print ("Vamos começar!")
+print()
+
+loser = input ("Qual é o significado da letra 'L' feita com a mão direita no símbolo de Glee?")
+if loser == "loser":
+    print ("Parabéns, você acertou! Bora para a próxima pergunta!")
+else:
+  print ("Tente novamente!")
+  print ()
+
+gleek = input("qual é o termo usado para caracterizar um fã de Glee?")
+if gleek == "gleek":
+    print ("Parabéns, você acertou!Bora para a próxima pergunta!")
+else:
+  print ("Tente novamente!")
+  print ()
