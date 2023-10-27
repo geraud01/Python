33f816fa63f29d6496a2ed44ee64a7d6e92176cb
Geraud <geraud.pereira@hotmail.com>
2023-10-27 09:28:12 -0300

    Desafio 100 dias

---
+++ Day_05.py
@@ -0,0 +1,78 @@
+print ("Qual é o seu signo?")
+print ()
+print ("Digite SIM ou NÃO para responder as perguntas.")
+print ()
+print ("Bora lá!")
+
+
+aries = input ("As pessoas costumas dizer que você é inpaciente?:")
+if aries == "sim":
+
+    print ("Você é do signo de Áries!")
+else:
+    print ("Entendi, vamos cotinuar!")
+touro = input ("As pessoas costumam dizer que você aprecia uma boa alimentão?:")
+if touro == "sim":
+    print ("Você é do signo de Touro!")
+else:
+    print ("Entendi, vamos cotinuar!") 
+gemios = input ("As pessoas costumam dizer que você têm uma sede insaciável de conhecimento e mergulham em tópicos complexos?:")
+if gemios == "sim":
+    print ("Você é do signo de Gêmeos!")
+else:
+    print ("Entendi, vamos cotinuar!")
+  
+cancer = input ("As pessoas costumam dizer que você é muito maternal e/ou carente?:")  
+if cancer == "sim":
+    print ("Você é do signo de Câncer!")
+else:
+    print ("Entendi, vamos cotinuar!")  
+  
+leao = input ("As pessoas costumam dizer que você é muito egoísta e/ou impulsiva?:")
+if leao == "sim":
+    print ("Você é do signo de Leão!")
+else:
+    print ("Entendi, vamos cotinuar!")  
+  
+virgem = input ("As pessoas costumam dizer que você é muito crítico e/ou observador?:")
+if virgem == "sim":
+    print ("Você é do signo de Virgem!")
+else:
+    print ("Entendi, vamos cotinuar!")  
+  
+libra = input ("As pessoas costumam dizer que você é muito indeciso?:")
+if libra == "sim":
+    print ("Você é do signo de Libra!")
+else:
+  print ("Entendi, vamos cotinuar!")
+  
+escorpiao = input ("As pessoas costumam dizer que você é muito vingativo?:")
+if escorpiao == "sim":
+    print ("Você é do signo de Escorpião!")
+else:
+    print ("Entendi, vamos cotinuar!")  
+  
+sagitario = input ("As pessoas costumam dizer que você não gosta de se sentir preso(a)?:")
+if sagitario == "sim":
+    print ("Você é do signo de Sagitário!")
+else:
+    print ("Entendi, vamos cotinuar!") 
+    
+capricornio = input ("As pessoas costumam dizer que você gosta de ter ou gastar seu dinheiro?:")
+if capricornio == "sim":
+    print ("Você é do signo de Capricornio!")
+else:
+    print ("Entendi, vamos cotinuar!")  
+  
+aquario = input ("As pessoas costumam dizer que você não se preocupa com as opiniões alheias a seu respeito?:")  
+if aquario == "sim":
+    print ("Você é do signo de Aquário!")
+else:
+    print ("Entendi, vamos cotinuar!")  
+  
+peixes = input ("As pessoas costumam dizer que você é muito romântico e sonhador?:")
+if peixes == "sim":
+    print ("Você é do signo de Peixes!")
+else:
+    print ("Entendi, vamos cotinuar!")  
+  
