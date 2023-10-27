47571503603c7c0d9dd4263500a63f6dfa799fb4
Geraud <geraud.pereira@hotmail.com>
2023-10-27 09:22:26 -0300

    Dia_01.py

---
+++ Dia_01.py
@@ -0,0 +1,9 @@
+print ("Hi, my name é Geraud Oliveira")
+print ("today's october 22th 2023")
+print ("""
+I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happend to be. No excuses for not coding from the middle of a field!
+""")
+print ("I am feeling freaking" + "\U0001F60A")
+print ("You can follow my progress at replit.com/@geraudpereira")
