4b8059f40842559aa097faec76d91470a8e9125c
Geraud <geraud.pereira@hotmail.com>
2023-10-27 09:31:36 -0300

    Desafio 100 dias

---
+++ Dia_08.py
@@ -0,0 +1,23 @@
+print ("Bem-vindos")
+print ()
+
+name = input ("Qual é o seu nome? ")
+print ()
+age = input ("Qual é a sua idade? ")
+print ()  
+city = input ("Qual é a sua cidade? ")
+print ()
+print ("Olá, então você se chama", name,"! Você tem", age, "anos e mora em", city,". É um prazer te conhecer")  
+print ()
+
+DOW = input("Que dia é hoje?")
+if DOW == "segunda" or DOW == "Segunda":
+   print("Tenho certeza que será uma otima segunda", name)
+if DOW == "terça" or DOW == "Terça":
+   print("Que tenha uma incrivel terça-feira", name)
+if DOW == "quarta" or DOW == "Quarta":
+   print("Feliz Hump Day", name)
+if DOW == "Quinta" or DOW == "quintay":
+   print(name,"sua semana está quase acabando!")
+if DOW == "sexta" or DOW == "Sexta":
+   print(name, "Sextou, bebê!")
