4e1d56998f38db8dc42ac396c4b6d4f4ce48e915
Geraud <geraud.pereira@hotmail.com>
2023-10-27 09:27:08 -0300

    Desafio 100 dias

---
+++ Dia_04.py
@@ -0,0 +1,16 @@
+print ("=== Welcome to your adveture simulator! ===")
+print (""" I'm going to ask you a nunch of questions and then creat an epic story with you as the star!
+""")
+print ()
+name = input ("What is your name? ")
+enemy = input ("What is your enemy's name? ")
+superPower = input ("What is your super power? ")
+live = input ( "Where do you live? ")
+food = input ("What is your favorite food? ")
+print ()
+print ("Here is your story: ")
+print ()
+print ("In the bustling city of", live , "there lived a young named", name, ".Unbeknownst to the world,", name , "possessed the extraordinary superpower of" ,superPower, ";the ability to move objects with mind. they eyes, a vivid shade of emerald green, sparkled with determination and kindness.", name,"'s favorite food was a unique delicacy known as ", food," a local specialty in", live, ". These magical pastries were said to bring good luck to anyone who tasted their sweet, tangy filling and flaky golden crust." "However,", name,"'s peaceful life took a drastic turn when her worst enemy, a cunning sorcerer named", enemy, ", arrived in",live,".", enemy, ", with his dark robes and sinister laugh, sought to harness the city's magical energy for his nefarious plans. He was determined to plunge the world into eternal darkness. As news of", enemy, "'s arrival spread, fear gripped", live, ". But", name, "armed with her telekinetic powers and unwavering courage, stepped forward to protect they city. With each wave of they hand, they repelled Malachar's dark spells, determined to save not only her favorite city but the entire realm from his malevolent grasp.In a dramatic showdown atop the city's ancient tower,", name ,"and", enemy ,"clashed. The air crackled with energy as their powers collided. With a burst of determination,", name ,"summoned all they strength and channeled her telekinetic abilities to create a barrier, deflecting Malachar's most powerful spell back at him.The sorcerer's eyes widened in disbelief as his own magic engulfed him, rendering him powerless.", live, "was saved, thanks to", name, "'s bravery and quick thinking. In the aftermath of the battle, the people of", live, "celebrated their hero.",  name, ",once an ordinary girl, had become a legend. And as a token of their gratitude, the city's bakers created a new delicacy in they honor:", name, ",'s Telekinetic Raspberry Tarts. These magical pastries became a symbol of hope and resilience, a reminder that even in the face of darkness, one person's courage could light up the world.")
+
+print ()
+print ("The end.")  
